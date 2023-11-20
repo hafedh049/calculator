846f1f5a145abaa5c7772dfaac22d5d2ccb74242
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-20 15:28:25 +0100

    A

---
+++ tree.c
@@ -0,0 +1,154 @@
+#include "tree.h"
+
+void inorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    inorderTraversal(root->left);
+    printf("\e[1;32m%d \e[1;33m-> \e[0m", root->data);
+    inorderTraversal(root->right);
+}
+
+void preorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    printf("\e[1;32m%d \e[1;33m-> \e[0m", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+void postorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("\e[1;32m%d \e[1;33m-> \e[0m", root->data);
+}
+
+TreeNode *createNode(int value)
+{
+    struct TreeNode *newNode = (TreeNode *)malloc(sizeof(TreeNode));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+void insert(TreeNode **root, int value)
+{
+
+    if (!(*root))
+    {
+        TreeNode *tmp = NULL;
+        tmp = (TreeNode *)malloc(sizeof(TreeNode));
+        tmp->left = tmp->right = NULL;
+        tmp->data = value;
+        *root = tmp;
+        return;
+    }
+    if (value < (*root)->data)
+        insert(&((*root)->left), value);
+    else if (value > (*root)->data)
+        insert(&((*root)->right), value);
+}
+
+TreeNode *search(TreeNode *root, int value)
+{
+    if (!(root))
+        return NULL;
+    if (value == root->data)
+        return root;
+    else if (value < root->data)
+        search(root->left, value);
+    else if (value > root->data)
+        search(root->right, value);
+}
+
+void delete_tree(TreeNode **root)
+{
+    if (*root)
+    {
+        delete_tree(&(*root)->left);
+        delete_tree(&(*root)->right);
+        free((*root));
+    }
+}
+
+TreeNode *findMinValueNode(TreeNode *node)
+{
+    if (node == NULL)
+        return NULL;
+
+    while (node->left != NULL)
+        node = node->left;
+
+    return node;
+}
+
+void deleteNode(TreeNode **root, int key)
+{
+    if (*root == NULL)
+        return;
+
+    if (key < (*root)->data)
+        deleteNode(&((*root)->left), key);
+    else if (key > (*root)->data)
+        deleteNode(&((*root)->right), key);
+    else
+    {
+        if ((*root)->left == NULL)
+        {
+            TreeNode *temp = (*root)->right;
+            free(*root);
+            *root = temp;
+        }
+        else if ((*root)->right == NULL)
+        {
+            TreeNode *temp = (*root)->left;
+            free(*root);
+            *root = temp;
+        }
+        else
+        {
+            TreeNode *temp = findMinValueNode((*root)->right);
+            (*root)->data = temp->data;
+            deleteNode(&((*root)->right), temp->data);
+        }
+    }
+}
+
+void printTree(TreeNode* root, int space) {
+    if (root == NULL) return;
+
+    space += 5;
+
+    printTree(root->right, space);
+
+    printf("\n");
+    for (int i = 5; i < space; i++)
+        printf(" ");
+    printf("\e[1;32m%d\e[0m", root->data);
+
+    printTree(root->left, space);
+}
+
+int countElements(TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    return 1 + countElements(root->left) + countElements(root->right);
+}
+
+int countLeftSubtreeElements(TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    return 1 + countLeftSubtreeElements(root->left);
+}
+
+int countRightSubtreeElements(TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    return 1 + countRightSubtreeElements(root->right);
+}
