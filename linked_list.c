467d403cb325ec390ba9952bd9ef3d09f033b54b
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-21 12:43:31 +0100

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -122,7 +122,7 @@ int isNumber(char *num)
     int i = 0;
     while (num[i])
     {
-        if ( ((*(num + i) == '-') && (*(num + i + 1))))
+        if ( ((*(num + i) == '-') && (isdigit(*(num + i + 1)))))
         {
             i += 1;
             continue;
